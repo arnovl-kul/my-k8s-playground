1639d66dc8df45dffe263ecfb29f5fc7b938e8c7
arno <arno.vanlangendonck@gmail.com>
2021-03-05 09:25:28 +0100

    added locust metrics collector

--- apps/flask-web-app/app/app.py
+++ apps/flask-web-app/app/app.py
@@ -7,25 +7,28 @@ app = Flask(__name__)
 
 app.debug = True
 
+INFLUXDB_HOST = 'monitoring-influxdb.kube-system.svc.cluster.local'
+INFLUXDB_PORT = 8086
+
 @app.route('/')
 def hello_thesis():
-    return "Hello, this is the home page of my thesis!"
+    return "Hello, this is the home page of my thesis! v1"
 
 @app.route('/create-influx')
 def create_influx():
-    client = InfluxDBClient(host='10.98.182.163', port=8086)  # Hard coded, could be improved
+    client = InfluxDBClient(host=INFLUXDB_HOST, port=INFLUXDB_PORT)  # Hard coded, could be improved
     client.create_database('flask-data')
     return "done"
 
 @app.route('/list-influx-dbs')
 def list_dbs():
-    client = InfluxDBClient(host='10.98.182.163', port=8086)
+    client = InfluxDBClient(host=INFLUXDB_HOST, port=INFLUXDB_PORT)
     print(client.get_list_database())
     return "done"
 
 @app.route('/post-influxdb/<float:current_value>')
 def post_new_val(current_value):
-    client = InfluxDBClient(host='10.98.182.163', port=8086)
+    client = InfluxDBClient(host=INFLUXDB_HOST, port=INFLUXDB_PORT)
     client.switch_database("flask-data")
     time = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
     json_body = [{
@@ -44,8 +47,11 @@ def post_new_val(current_value):
 
 @app.route('/get-vals')
 def get_values():
-    client = InfluxDBClient(host='10.98.182.163', port=8086)    
+    client = InfluxDBClient(host=INFLUXDB_HOST, port=INFLUXDB_PORT)    
     client.switch_database("flask-data")
     results = client.query('SELECT * FROM "flask-data"."autogen"."sampleEvents" WHERE time > now() - 4d')
     print(results.raw)
     return results.raw
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
--- apps/locust-influx-metrics/collector.py
+++ apps/locust-influx-metrics/collector.py
@@ -28,12 +28,26 @@ class Collector:
         self.session = session
         self.sock = socker.socket()
         self.client = InfluxDBClient(host=INFLUXDB_HOST, port=INFLUXDB_PORT, timeout=5)
+        self.client.switch_database("flask-data")
 
         print("Checking connection to influxdb... wait max 15 seconds")
         try:
             v = client.ping()
         except:
             exit("Problem with connection to Influxdb, check ip and port, aborting")
+
+    def push_current_metrics(self, userCount, rps, medianRespTime):
+        time = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+        json_body = [{
+            "measurement": "responseData",
+            "time": time,
+            "fields": {
+                "userCount": count,
+                "rps": rps,
+                "medianRespTime": medianRespTime
+            }
+        }]
+        client.write_points(json_body)
 
     def push_metrics_user_count(self, count):
         time = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
@@ -49,10 +63,10 @@ class Collector:
     def push_metrics_rps(self, rps):
         time = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
         json_body = [{
-            "measurement": "userCount",
+            "measurement": "rps",
             "time": time,
             "fields": {
-                "userCount": count
+                "rps": count
             }
         }]
         client.write_points(json_body)
@@ -67,6 +81,34 @@ class Collector:
     async def __call__(self):
         resp = await self.fetch(LOCUST_HOST+'/stats/requests')
         user_count = json.loads(resp)['user_count']
-        self.push_metrics_user_count(user_count)
+        status = json.load(resp)['state']
 
-        status = json.load
+        if (status == "running" and users != 0):
+            rps = json.loads(resp)['total_rps']
+        else:
+            rps = 0
+
+        median = json.loads(resp)['current_response_time_percentile_50']
+
+        self.push_current_metrics(user_count, rps, median)
+
+async def _constant_pooling(loop):
+    session = aiohttp.ClientSession(loop=loop)
+    a = Collector(loop=loop,session=session)
+
+    while True:
+        try:
+            await a()
+        except:
+            await asyncio.sleep(5, loop=loop)
+        await asyncio.sleep(3, loop=loop)
+
+def main():
+    loop = asyncio.get_event_loop()
+
+    loop.create_task(_constant_pooling(loop=loop))
+
+    loop.run_forever()
+
+if __name__ == '__main__':
+    main()
