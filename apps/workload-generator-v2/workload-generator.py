2828c1ae13254b279acd44582a049ba0b4a1b9fd
arno <arno.vanlangendonck@gmail.com>
2021-03-11 11:19:47 +0100

    fixed indent?

--- apps/workload-generator-v2/workload-generator.py
+++ apps/workload-generator-v2/workload-generator.py
@@ -3,7 +3,8 @@
 # Generates a workload using locust, sends data to INFLUXDB for further analysis
 
 import sys
-import argparse, textwrap
+import argparse
+import textwrap
 import os.path
 import yaml
 import time
@@ -27,10 +28,14 @@ parser.add_argument(
         '''
     )
 )
-parser.add_argument("-f", "--file",             action='store', help="File containing a trace")
-parser.add_argument("-l", "--lhost",      action='store', help="Locust web endpoint")
-parser.add_argument("-i", "--dbip",      action='store', help="IP address of the influx-db")
-parser.add_argument("-p", "--dbport",    action='store', help="Port of influx-db")
+parser.add_argument("-f", "--file",             action='store',
+                    help="File containing a trace")
+parser.add_argument("-l", "--lhost",      action='store',
+                    help="Locust web endpoint")
+parser.add_argument("-i", "--dbip",      action='store',
+                    help="IP address of the influx-db")
+parser.add_argument("-p", "--dbport",    action='store',
+                    help="Port of influx-db")
 
 args = parser.parse_args()
 
@@ -48,16 +53,19 @@ try:
 except:
     exit("Problem with connection to Influxdb, check ip and port, aborting")
 
+
 def stop_load():
     r = requests.get(LOCUST_HOST+"/stop")
+
 
 def abort_experiment(msg):
     stop_load()
     exit("Program aborted: " + msg)
 
+
 def set_user_count(count):
     url = LOCUST_HOST + "/swarm"
-    r = requests.post(url, data = {'user_count': count, 'spawn_rate': count})
+    r = requests.post(url, data={'user_count': count, 'spawn_rate': count})
 
     if (r.status_code == 200):
         time = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
@@ -70,8 +78,9 @@ def set_user_count(count):
         }]
         client.write_points(json_body)
 
-def check_params(segment_type,initial_count,end_count,duration):
-	
+
+def check_params(segment_type, initial_count, end_count, duration):
+
 	try:
 		assert initial_count >= 0
 		assert end_count >= 0
@@ -80,58 +89,60 @@ def check_params(segment_type,initial_count,end_count,duration):
 		assert end_count == int(end_count)
 		assert duration == int(duration)
 	except:
-		abort("Invalid params in config file, counts and duration need to be positive integers")	
-	
-	if(segment_type=='stable'):
+		abort("Invalid params in config file, counts and duration need to be positive integers")
+
+	if(segment_type == 'stable'):
 		try:
-			assert initial_count==end_count
-		except: 
+			assert initial_count == end_count
+		except:
 			abort('Invalid configuration. Stable segments need to have the same initial and end count')
 
-	elif(segment_type=='rising'):		
+	elif(segment_type == 'rising'):
 		try:
-			assert initial_count<end_count
-		except: 
+			assert initial_count < end_count
+		except:
 			abort('Invalid configuration. Rising segments need to have the initial count smaller than the end count')
 
-	elif(segment_type=='decreasing'):		
+	elif(segment_type == 'decreasing'):
 		try:
-			assert initial_count>end_count
-		except: 
+			assert initial_count > end_count
+		except:
 			abort('Invalid configuration. Decreasing segments need to have the initial count greater than the end count')
 
 	else:
 		abort('Invalidad segment type. Options are: stable, rising, decreasing')
 
+
 def process_segment(trace):
-	segment_type=trace['segment']
-	initial_count=trace['initialCount']
-	end_count=trace['endCount']
-	duration=trace['duration']
+	segment_type = trace['segment']
+	initial_count = trace['initialCount']
+	end_count = trace['endCount']
+	duration = trace['duration']
 
-	check_params(segment_type,initial_count,end_count,duration)
+	check_params(segment_type, initial_count, end_count, duration)
 
-	if(segment_type=='stable'):
+	if(segment_type == 'stable'):
 		set_user_count(initial_count)
 		time.sleep(duration)
 
-	elif(segment_type=='rising'):
-		times=end_count-initial_count
-		delay=duration/times
+	elif(segment_type == 'rising'):
+		times = end_count-initial_count
+		delay = duration/times
 
 		for t in range(times+1):
 			set_user_count(t+initial_count)
 			time.sleep(delay)
 		# set_user_count(end_count)
 
-	elif(segment_type=='decreasing'):
-		times=initial_count-end_count
-		delay=duration/times
+	elif(segment_type == 'decreasing'):
+		times = initial_count-end_count
+		delay = duration/times
 
 		for t in range(times+1):
 			set_user_count(initial_count-t)
 			time.sleep(delay)
-		# set_user_count(end_count)	
+		# set_user_count(end_count)
+
 
 def generate_load():
 	if(file_exists(CONFIG_FILE)):
@@ -142,9 +153,9 @@ def generate_load():
 			sys.exit(1)
 	else:
 		print("File does not exists. Please try again...")
-		sys.exit(1)		
+		sys.exit(1)
 
-	traces=config_data['load']
+	traces = config_data['load']
 
 	time = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
     json_body = [{
