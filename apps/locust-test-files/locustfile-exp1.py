4f90ae88e492fa011ffdfaf9f82ec94ae4a976d7
arno <arno.vanlangendonck@gmail.com>
2021-07-01 17:28:33 +0200

    locust rewrite

--- apps/locust-test-files/locustfile-exp1.py
+++ apps/locust-test-files/locustfile-exp1.py
@@ -3,18 +3,19 @@ import time
 import socket
 import atexit
 
-class TasksetT1(TaskSet):
-    # one can specify tasks like this
-    #tasks = [index, stats]
+DEBUG = True
+APPLICATION_HOST = "http://172.19.133.29:30698"
+INFLUXDB_HOST = '172.19.133.29'
+INFLUXDB_PORT = 30421
+DATABASE = "gold-app-data"
 
+class TasksetT1(TaskSet):
     def on_start(self):
         self.client.get("/login")
         
     def on_stop(self):
         self.client.get("/logout")
-    
 
-    # but it might be convenient to use the @task decorator
     @task
     def pushJob(self):
         with self.client.get("/pushJob/10",name="gold", catch_response=True) as resp:
@@ -23,43 +24,45 @@ class TasksetT1(TaskSet):
 
 class MyUser(HttpUser):
     weight = 1
-  
-    # host = "http://demo.gold.svc.cluster.local:80
-    host = "http://172.19.133.30:30698"
-
     wait_time = between(0,0)  
-    
-    #task_set = TasksetT1
     tasks = [TasksetT1]
+    sock = socket.socket() # -> graphite
 
-    sock = socket.socket()
+    self.idb = InfluxDBClient(host=INFLUXDB_HOST, port=INFLUXDB_PORT, timeout=5)
+    self.idb.switch_database(DATABASE)
+
+    if DEBUG:
+        print("Checking connection to influxdb... wait max 15 seconds")
     try:
-        sock.connect(('172.19.133.30', 30689))
-    except (socket.error):
-        print("Couldnt connect with the socket-server: terminating program...")
+        v = self.client.ping()
+        if DEBUG:
+            print('Succesfully pinged InfluxDB')
+    except:
+        exit("Problem with connection to Influxdb, check ip and port, aborting")
 
-#    def __init__(self, parent):
-#        super().__init__(parent)
-#        self.sock = socket.socket()
-#        try:
-#            self.sock.connect(('172.17.13.119', 30689))
-#        except (socket.error):
-#            print("Couldnt connect with the socket-server: terminating program...")
-        #events = self.environment.events
-        #events.request_success += self.hook_request_success
-        #events.request_failure += self.atexit.register(self.exit_handler)
     @events.request_success.add_listener
     def hook_request_success(request_type, name, response_time, response_length, **kw):
-        data_latency="%s %d %d\n" % ("performance." + name.replace('.', '-')+'.latency', response_time,  time.time())
-        # data_request="%s %d %d\n" % ("performance." + name.replace('.', '-')+'.requests', 1,  time.time())
-        MyUser.sock.send(data_latency.encode())
-        # self.sock.send(data_request.encode())
+        if DEBUG:
+            print(str(time.time()) + " - reqType: " + str(request_type) + ", name: " + str(name) + ", respTime: " + str(response_time) + ", respLength: " + str(response_length))
+        if str(name) == 'gold':
+            self.write_idb(response_time)
+
+    def write_idb(t):
+        time = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
+        json_body = [{
+            "measurement": "response_time",
+            "time": time,
+            "fields": {
+                "response_time": t
+            }
+        }]
+        self.idb.write_points(json_body)
 
     def hook_request_fail(self, request_type, name, response_time, exception):
+        if DEBUG:
+            print(str(time.time()) + " - FAILED - reqType: " + str(request_type) + ", name: " + str(name) + ", respTime: " + str(response_time) + ", EXCEPTION: " + str(exception))
         self.request_fail_stats.append([name, request_type, response_time, exception])
     
     @events.test_stop.add_listener
     def exit_handler(**kw):
-        MyUser.sock.shutdown(socket.SHUT_RDWR)
-        MyUser.sock.close()
-
+        self.idb.close()
